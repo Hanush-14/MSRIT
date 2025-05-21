03d64795484e8864f923c9ae12bbe49aec52fde3
Hanush-14 <hanushhanu1014@gmail.com>
2025-05-21 14:57:41 +0530

    Update mat.c

--- daaa/mat.c
+++ daaa/mat.c
@@ -1,73 +1,60 @@
-#include<stdio.h>
-void main()
-{
+// Matrix Addtion and Substraction
+#include <stdio.h>
+#include <time.h>
 
-	int i,j,m,n;
-	printf("enter the size of 1st matrix\n");
-	scanf("%d",&m);
-	printf("enter the size of 2nd matrix\n");
-        scanf("%d",&n);
-	int a[m][n],a1[m][n];
-	printf("enter the 1st matrix elemenets\n");
-	for(i = 0;i<m;i++)
-	{
-		for(j = 0;j<n;j++)
-		{
-			scanf("%d",&a[i][j]);
-		}
-	}
-	printf("enter the 2nd matrix elemenets\n");
-	for(i = 0;i<m;i++)
-	{
-		for(j=0;j<n;j++)
-		{
-			scanf("%d",&a1[i][j]);
-		}
-	}
-	int c[m][n];
-	printf("mul of two array elemenets are \n");
-	for(i = 0;i<m;i++)
-	{
-		for(j=0;j<n;j++)
-		{
-			for(int k =0;k<m;k++)
-			{
-				c[i][j] =a[i][k]*a1[k][j];
-			}
-		}
-	}
-	int d[m][n];
-        printf("mul of two array elemenets are \n");
-        for(i = 0;i<m;i++)
-        {
-                for(j=0;j<n;j++)
-                {
-                        for(int k =0;k<n;k++)
-                        {
-                                d[i][j] =a[i][k]*a1[k][j];
-                        }
-                }
-        }
-	int r[m][n];
-        printf("result of two array elemenets are \n");
-        for(i = 0;i<m;i++)
-        {
-                for(j=0;j<n;j++)
-                {
-                                r[i][j] =c[i][j]*d[i][j];
-                }
-        }
+#define MAX_SIZE 10
 
-
-
-	for(i =0;i<m;i++)
-	{
-		for(j=0;j<n;j++)
-		{
-			printf("%d\t",r[i][j]);
-		}
-		printf("\n");
-	}
+void matrixAddition(int A[MAX_SIZE][MAX_SIZE], int B[MAX_SIZE][MAX_SIZE], int result[MAX_SIZE][MAX_SIZE], int rows, int cols) {
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < cols; j++)
+            result[i][j] = A[i][j] + B[i][j];
 }
 
+void matrixSubtraction(int A[MAX_SIZE][MAX_SIZE], int B[MAX_SIZE][MAX_SIZE], int result[MAX_SIZE][MAX_SIZE], int rows, int cols) {
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < cols; j++)
+            result[i][j] = A[i][j] - B[i][j];
+}
 
+void printMatrix(int matrix[MAX_SIZE][MAX_SIZE], int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++)
+            printf("%d ", matrix[i][j]);
+        printf("\n");
+    }
+}
+
+int main() {
+    int A[MAX_SIZE][MAX_SIZE], B[MAX_SIZE][MAX_SIZE], result[MAX_SIZE][MAX_SIZE];
+    int rows, cols;
+    clock_t start, end;
+
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+
+    printf("Enter matrix A:\n");
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < cols; j++)
+            scanf("%d", &A[i][j]);
+
+    printf("Enter matrix B:\n");
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < cols; j++)
+            scanf("%d", &B[i][j]);
+
+    start = clock();
+    matrixAddition(A, B, result, rows, cols);
+    end = clock();
+    printf("\nMatrix Addition:\n");
+    printMatrix(result, rows, cols);
+    printf("Time: %.6lf seconds\n", ((double)(end - start)) / CLOCKS_PER_SEC);
+
+    start = clock();
+    matrixSubtraction(A, B, result, rows, cols);
+    end = clock();
+    printf("\nMatrix Subtraction:\n");
+    printMatrix(result, rows, cols);
+    printf("Time: %.6lf seconds\n", ((double)(end - start)) / CLOCKS_PER_SEC);
+
+    return 0;
+}

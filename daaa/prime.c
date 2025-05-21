928eca7cf016547386169ccbb2e0e7bb13a3cf10
Hanush-14 <hanushhanu1014@gmail.com>
2025-05-21 14:58:14 +0530

    Update prime.c

--- daaa/prime.c
+++ daaa/prime.c
@@ -1,20 +1,33 @@
-#include<stdio.h>
+// Check given number is prime or not
+#include <stdio.h>
+#include <time.h>
 
-void main()
-{
-	int n,i,flag=0,count=0;
-	printf("Enter the number to be checked\n");
-	scanf("%d",&n);
-	for(i=1;i<=n;i++)
-	{
-		if(n%i==0)
-		{
-			count=count+1;
-		}
-	}
-	if(count == 2)
-		printf("The given number is prime\n");
-	else
-		printf("The given number is not a prime\n");
+int isPrime(int num) {
+    if (num <= 1) return 0;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) return 0;
+    }
+    return 1;
+}
 
+int main() {
+    int num;
+    clock_t start, end;
+    double time_taken;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    start = clock();
+    if (isPrime(num)) {
+        printf("%d is prime.\n", num);
+    } else {
+        printf("%d is not prime.\n", num);
+    }
+    end = clock();
+
+    time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("Time taken: %lf seconds\n", time_taken);
+
+    return 0;
 }

784cc04ac8f51c61c9560bc90660cf567ece1c42
Hanush-14 <hanushhanu1014@gmail.com>
2025-05-21 14:56:43 +0530

    Update linearSearch.c

--- daaa/linearSearch.c
+++ daaa/linearSearch.c
@@ -1,36 +1,30 @@
-#include<stdio.h>
+// Linear Search Algorithm and Time taken to Search an element
+#include <stdio.h>
+#include <time.h>
 
-void main()
+int main() {
+    int n, key, i;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
 
-{
+    int arr[n];
+    printf("Enter the elements: ");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
 
-int ele,flag=0,pos=-1,n;
+    printf("Enter element to search: ");
+    scanf("%d", &key);
 
-printf("enter the array size \n");
-scanf("%d",&n);
+    clock_t start = clock();
+    for (i = 0; i < n; i++) {
+        if (arr[i] == key) break;
+    }
+    clock_t end = clock();
 
-int a[n];
-for(int i=0;i<n;i++)
-{
-scanf("%d",&a[i]);
-}
+    if (i < n) printf("Element found at index %d\n", i);
+    else printf("Element not found\n");
 
-printf("Enter the searching element \n");
-scanf("%d",&ele);
-
-for(int i=0;i<n;i++){
-	if(ele == a[i])
-		{
-			//printf("The element found %d \n",a[i]);
-			flag=1;
-			pos=i;
-		}
-}
-
-if(flag == 1)
-	printf("The elemenet found at %d\n",pos);
-else
-	printf("The elemenet not found \n");
-
-
+    printf("Time taken: %f seconds\n", (double)(end - start) / CLOCKS_PER_SEC);
+    return 0;
 }
